3bf5b2319b21815ac1109dc56a8fdb8060dce4b9
Marie <mariiao98@wizardsdev.com>
2020-05-17 21:23:55 +0300

    something

---
+++ components/request/Request.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Request: React.FC = () => {
+  return(
+        <div>Request will be here</div>
+  );
+};
+
+export default Request;
---
+++ constans/config/headerItem.ts
@@ -0,0 +1,5 @@
+export default [
+    { idItem: 'requestItem', resourcesKey: 'requestItem', name: 'Request', path: '/request',  onClickCallback: 'handleRouteRequest' },
+    { idItem: 'battlesItem', resourcesKey: 'battlesItem', name: 'Battles', path: '/battles', onClickCallback: 'handleRouteBattles' },
+    { idItem: 'storyItem', resourcesKey: 'storyItem', name: 'Story', path: '/story', onClickCallback: 'handle|RouteStory' },
+];
