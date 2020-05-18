d996e78181111bbeaae87b948b9c5d786321b29c
Andrey <andreybe@wizardsdev.com>
2020-05-18 11:56:57 +0300

    add chart component to project

---
+++ components/chart/styledComponent.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import colorMatrix from '../../constants/colorMatrix';
+
+export const ChartWrapper = styled.div`
+  background: ${colorMatrix.black};
+  opacity: 0.85;
+  width: 100%;
+`;
