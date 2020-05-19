1d908f6e6031d2b73a82a9ad91910c61b44c8e8a
Marie <mariiao98@wizardsdev.com>
2020-05-19 09:12:17 +0300

    fix navbar

--- components/header/appButtonBlock/styledComponents.ts
+++ components/header/appButtonBlock/styledComponents.ts
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
 import color from '../../../constans/colorMatrix';
+import fonts from '../../../constans/fonts';
 
 export const AppButtonBlockWrapper = styled.div`
   display:flex;
   flex-direction: row;
   justify-content: flex-end;
-  text-align: center;
   `;
 
 export const ButtonWrapper = styled.div`
   height: 50%;
   margin: 10px;
-  font-family: PT Serif;
+  font-family: ${fonts.header};
   text-align: center;
   `;
 
